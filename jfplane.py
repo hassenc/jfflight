3538a954a77cdb1513db5035a0644c2284a6ede3
hassenc <hassen.chaieb@telecom-bretagne.eu>
2017-05-30 19:52:29 +0200

    Add Holidays

--- jfplane.py
+++ jfplane.py
@@ -1,31 +1,65 @@
 #!/usr/bin/python
 from datetime import datetime
 from datetime import timedelta
+
+
 print (datetime.now()).weekday()
 print (datetime.now() + timedelta(days=2)).weekday()
-#Add 1 day
-print (datetime.now() + timedelta(days=1)).date()
-print (datetime.now() + timedelta(days=2) - timedelta(days=1)).date()
-print (datetime.now() + timedelta(days=1)).date() == (datetime.now() + timedelta(days=2) - timedelta(days=1)).date()
 
-import time
+HOLIDAYS = ["2017-07-14"]
 
-now = time.strftime("%c")
-print now
-time.strftime("%w")
-time +
+def isWeekend(date):
+  if (date.weekday() == 5) or (date.weekday() == 6):
+    return True
+  else:
+    return False
 
+def isHoliday(date):
+  for holiday in HOLIDAYS:
+    if date.date() == datetime.strptime(holiday, "%Y-%m-%d").date():
+      return True
+  return False
 
+def isWorkable(date):
+  if (isHoliday(date) or isWeekend(date)):
+    return False
+  else:
+    return True
+
+def getMaxSpan(date):
+  span = 0
+  nextDay = date
+  while isWorkable(nextDay) is False:
+    span = span + 1
+    nextDay = nextDay + timedelta(days=1)
+  return span
+
+def getEndDates(date):
+  endDates = []
+  endDates.append((date + timedelta(days=2)).date())
+  endDates.append((date + timedelta(days=3)).date())
+  maxSpan = getMaxSpan(date)
+  if maxSpan > 3:
+    print "maxSpan superior"
+    print maxSpan
+    endDates.append((date + timedelta(days=maxSpan)).date())
+  print "endDates"
+  print endDates
+  return endDates
 
 def dates(endDate):
   result = []
   date = {"start":datetime.now(), "end":datetime.now()}
   today = datetime.now()
   date = today
-  while (date.date() != endDate):
+  i = 0
+  while ((date.date() != endDate) and (i<3)):
+    i = i+1
+    print type(date.date())
+    for endDate in getEndDates(date):
+      result.append({"start":date.date(), "end":endDate})
     date = date + timedelta(days=1)
-    result.append(date)
   return result
 
 
-dates("2017-06-01")
+print dates(datetime.strptime("2017-07-15", "%Y-%m-%d").date())
